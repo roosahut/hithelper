12c10c8f17fab011df90f5693db0305cb4294cf2
kemppainensani <kemppainensani@gmail.com>
2024-10-21 11:41:38 +0300

    docstring to get song lyrics

--- backend/data/get_song_lyrics.py
+++ backend/data/get_song_lyrics.py
@@ -9,6 +9,16 @@ load_dotenv()
 genius_token = os.getenv('GENIUS_TOKEN')
 
 class Song:
+    """
+        Initializes a Song object.
+
+        Args:
+            rank (int): The rank of the song.
+            title (str): The title of the song.
+            artist (str): The artist of the song.
+            lyrics (str, optional): The lyrics of the song. Defaults to an empty string.
+            genre (list, optional): The genre(s) of the song. Defaults to an empty list.
+    """
     def __init__(self, rank: int, title: str, artist: str, lyrics: str = "", genre: list=[]):
         self.rank = rank
         self.title = title
@@ -18,6 +28,12 @@ class Song:
 
 
 def read_songs_from_csv() -> List[Song]:
+    """
+    Reads songs from a CSV file and returns a list of Song objects.
+
+    Returns:
+        List[Song]: A list of Song objects.
+    """
     songs = []
     with open("./backend/data/top_100_songs.csv", mode='r') as file:
         reader = csv.DictReader(file)
@@ -27,6 +43,12 @@ def read_songs_from_csv() -> List[Song]:
     return songs
 
 def get_song_lyrics_from_genius(songs: List[Song]):
+    """
+    Fetches lyrics and genres for a list of songs from the Genius API.
+
+    Args:
+        songs (List[Song]): A list of Song objects.
+    """
     genius = lyricsgenius.Genius(genius_token)
     for song in songs:
         song_data = genius.search_song(song.title, song.artist)
@@ -51,6 +73,6 @@ def write_songs_to_csv(songs: List[Song]):
         for song in songs:
             writer.writerow([song.rank, song.title, song.artist, song.genre, song.lyrics])
 
-#top_songs = read_songs_from_csv()
-#get_song_lyrics_from_genius(top_songs)
-#write_songs_to_csv(top_songs)
+top_songs = read_songs_from_csv()
+get_song_lyrics_from_genius(top_songs)
+write_songs_to_csv(top_songs)
